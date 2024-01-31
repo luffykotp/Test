def3b97811721b1c474d096cb8242fac58b6e509
Adrian <luffyd375@gmail.com>
2024-01-31 15:33:03 -0500

    Add Frame

--- window.py
+++ window.py
@@ -5,7 +5,12 @@ root = tk.Tk()
 stats = tk.Label(root, text="stats")
 firstButton = tk.Button(root, text= "Click me")
 
+frameOne = tk.LabelFrame(root, text = "Log in", padx =  100, pady = 100)
+login = tk.Label(frameOne, text = "Your name will go here.")
 
+
+frameOne.pack()
+login.pack()
 stats.pack()
 firstButton.pack()
 
