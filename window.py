d4d82e9aa06da165c20ad2c99b544addf17d4480
Adrian <luffyd375@gmail.com>
2024-01-31 15:00:25 -0500

    Add Button

--- window.py
+++ window.py
@@ -2,10 +2,12 @@ import tkinter as tk
 
 root = tk.Tk()
 
-stats = tk.Label(root, text="stas")
+stats = tk.Label(root, text="stats")
+firstButton = tk.Button(root, text= "Click me")
+
 
 stats.pack()
-
+firstButton.pack()
 
 
 
